712f32da8ac6c2282b21dea539013f05914b2e48
amonak-maker <jeanpkokora@gmail.com>
2019-12-24 02:37:30 +0100

    first commit

---
+++ src/app/edit-nav/edit-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-nav',
+  templateUrl: './edit-nav.component.html',
+  styleUrls: ['./edit-nav.component.scss']
+})
+export class EditNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
